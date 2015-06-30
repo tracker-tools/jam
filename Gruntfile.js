01f140bba22899e8a81cc223f0abd684a7406df6
Edward Hotchkiss <edward@edwardhotchkiss.com>
2015-06-29 20:48:16 -0700

    [proper theme]

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function(grunt) {
       },
       development: {
         files: [{
-          src: ['less/index.less'],
+          src: ['less/jam.less'],
           dest: 'css/jam.css'
         }]
        }
